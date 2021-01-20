5db68750c9016d042625a6f6bf8e85558416cd81
Gilson <gilsonjuniorpro@gmail.com>
2021-01-19 19:54:48 -0500

    added fibonacci sequence

---
+++ app/src/test/java/com/apptesting/ca/HomeworkTest.kt
@@ -0,0 +1,15 @@
+package com.apptesting.ca
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class HomeworkTest {
+
+    @Test
+    fun `check fib`(){
+        val result = Homework.fib(9)
+
+        var res = result == 34L
+        assertThat(res).isTrue()
+    }
+}
